6a2f26daa28ae181ac77e712e59f049bf168ba53
Дмитрий Шагаров <dashagarov@edu.hse.ru>
2021-12-11 00:13:55 +0300

    build added

---
+++ Task5/build/CMakeFiles/Task5.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/eva/Desktop/AoCS-threads/Task5/entities/Island.cpp" "/home/eva/Desktop/AoCS-threads/Task5/build/CMakeFiles/Task5.dir/entities/Island.cpp.o"
+  "/home/eva/Desktop/AoCS-threads/Task5/entities/PirateGang.cpp" "/home/eva/Desktop/AoCS-threads/Task5/build/CMakeFiles/Task5.dir/entities/PirateGang.cpp.o"
+  "/home/eva/Desktop/AoCS-threads/Task5/main.cpp" "/home/eva/Desktop/AoCS-threads/Task5/build/CMakeFiles/Task5.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
